2150c4a116aa42457eac8339dad6d5036bcaf563
Alexander <154324-alexander-rykov@users.noreply.gitlab.com>
2022-02-09 20:03:47 +0300

    Enable nullable reference types.

--- LookingForChars/CharsCounter.cs
+++ LookingForChars/CharsCounter.cs
@@ -10,7 +10,7 @@ namespace LookingForChars
         /// <param name="str">String to search.</param>
         /// <param name="chars">One-dimensional, zero-based <see cref="Array"/> that contains characters to search for.</param>
         /// <returns>The number of occurrences of all characters.</returns>
-        public static int GetCharsCount(string str, char[] chars)
+        public static int GetCharsCount(string? str, char[]? chars)
         {
             // TODO #1. Implement the method using "for" statement.
             throw new NotImplementedException();
@@ -24,7 +24,7 @@ namespace LookingForChars
         /// <param name="startIndex">A zero-based starting index of the search.</param>
         /// <param name="endIndex">A zero-based ending index of the search.</param>
         /// <returns>The number of occurrences of all characters within the specified range of elements in the <see cref="string"/>.</returns>
-        public static int GetCharsCount(string str, char[] chars, int startIndex, int endIndex)
+        public static int GetCharsCount(string? str, char[]? chars, int startIndex, int endIndex)
         {
             // TODO #2. Implement the method using "while" statement.
             throw new NotImplementedException();
@@ -39,7 +39,7 @@ namespace LookingForChars
         /// <param name="endIndex">A zero-based ending index of the search.</param>
         /// <param name="limit">A maximum number of characters to search.</param>
         /// <returns>The limited number of occurrences of characters to search for within the specified range of elements in the <see cref="string"/>.</returns>
-        public static int GetCharsCount(string str, char[] chars, int startIndex, int endIndex, int limit)
+        public static int GetCharsCount(string? str, char[]? chars, int startIndex, int endIndex, int limit)
         {
             // TODO #3. Implement the method using "do..while" statements.
             throw new NotImplementedException();
